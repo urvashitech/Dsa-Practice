1104e28be71d59d364c9e8527af7cfdc0d381b4d
urvashi <urvashikshatriya9@gmail.com>
2025-07-10 22:43:42 +0530

    making streak

---
+++ step1/09.cpp
@@ -0,0 +1,6 @@
+/* You are given a string S consisting of a list of words; the string contains lowercase alphabets and some special symbols such as (*, $, #). Now, our task is to make the string good, and this is done by removing all the special characters from the sentence. For this, we have to perform two operations-
+
+Firstly, choose the special symbol in the given sentence.
+In the second step, remove the special symbol and the closest alphabets present to the right of that special character.
+*/
+
