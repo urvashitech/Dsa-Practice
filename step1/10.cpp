d7f44362b2a5348c187b147ce054ae72d34427ea
urvashi <urvashikshatriya9@gmail.com>
2025-07-11 22:43:39 +0530

    new file is added

---
+++ step1/10.cpp
@@ -0,0 +1,5 @@
+/* In a field represented by a N*M matrix, certain cells contain crops, represented by the character 'C', while other cells are empty, represented by the character 'E'. To protect the crops from pests, pesticide can only be applied to cells containing crops.
+
+When pesticide is applied to a cell, it also protects all adjacent cells in the eight possible directions (up, down, left, right, and the four diagonals).
+
+Return the minimum number of pesticide applications required to ensure that all crops in the field are protected.  */
