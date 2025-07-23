19dd90b14edd05f4696089c7466573d05032c35d
urvashi <urvashikshatriya9@gmail.com>
2025-07-23 17:54:57 +0530

    mergeSort completed

--- step2/merge.cpp
+++ step2/merge.cpp
@@ -1,1 +1,58 @@
 //Given an array of size n, sort the array using Merge Sort.
+
+/* Problem Statement: Given an array of N integers, write a program to implement the Bubble Sorting algorithm. */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+void merge(vector<int> &arr , int st, int mid, int end){
+  vector<int> temp;
+  int i= st , j = mid+1;
+  while(i<=mid && j <=end){
+    if (arr[i] <= arr[j])
+    {
+        temp.push_back(arr[i]);
+        i++;
+    }
+    else{
+        temp.push_back(arr[j]);
+        j++;
+    }
+    while (i <= mid){
+        temp.push_back(arr[i]);
+        i++;
+    }
+    while (j<=end)
+    {
+        temp.push_back(arr[j]);
+        j++;
+    }
+    for (int i = 0; i < temp.size(); i++)
+    {
+        arr[i+st] = temp[i];
+    }
+    
+    
+    
+  }
+
+
+}
+void mergeSort(vector<int> &arr,int st,int end){
+    if(st < end){
+        int mid = st + (end-st)/2;
+
+        mergeSort(arr,st,mid);
+        mergeSort(arr,mid+1,end);
+        merge(arr,st,mid,end);
+    }
+}
+
+int main(){
+    int n = 6 ;
+    int arr[] = {13,46,24,52,20,9};
+   
+    return 0;
+}
